2127ca52d7d525ff8b9c277bdfbeda70c14477f4
Parker Moore <parkrmoore@gmail.com>
2014-01-08 13:47:18 -0800

    Symlink .env file; remove index.html file in public

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@ set :application, "janky"
 set :repository,  "git://github.com/parkr/janky-passenger.git"
 set :deploy_to,   "/var/www/#{application}"
 
+set :linked_files, %w{.env}
+
 set :scm, :git
 
 role :web, ENV['CAP_DEPLOY_HOST']
 role :app, ENV['CAP_DEPLOY_HOST']
 role :db,  ENV['CAP_DEPLOY_HOST']
-
-set :linked_files, %w{.env}
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -21,3 +21,11 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+
+namespace :deploy do
+  task :symlink_shared do
+    run "ln -s #{shared_path}/.env #{release_path}/"
+  end
+end
+
+before "deploy:restart", "deploy:symlink_shared"
